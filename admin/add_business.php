34d10ed0321e47b56b9e3e1203fc260b22e26355
Siddhi Verma <160858270+sid2564@users.noreply.github.com>
2026-04-23 13:59:41 +0530

    Update add_business.php

--- admin/add_business.php
+++ admin/add_business.php
@@ -66,7 +66,7 @@ if(isset($_POST['name'])){
             }
         }
 
-        header("Location: ../index.php?added=1");
+        header("Location: ../add_business.php?added=1");
         exit();
 
     } else {
