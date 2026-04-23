21309df4841b2d2536bfbab261a28ba1da80c031
Siddhi Verma <160858270+sid2564@users.noreply.github.com>
2026-04-23 15:57:42 +0530

    Update update_business.php

--- update_business.php
+++ update_business.php
@@ -2,23 +2,21 @@
 session_start();
 include "db.php";
 
-
-
 if(!isset($_SESSION['user_id'])){
     die("Login required");
 }
 
 $user_id = $_SESSION['user_id'];
 
-$id = $_POST['id'];
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$city = $_POST['city'];
-$description = $_POST['description'];
+// Variable values ko 'escape' karna zaroori hai taaki apostrophe (') se error na aaye
+$id = mysqli_real_escape_string($conn, $_POST['id']);
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$phone = mysqli_real_escape_string($conn, $_POST['phone']);
+$city = mysqli_real_escape_string($conn, $_POST['city']);
+$description = mysqli_real_escape_string($conn, $_POST['description']);
 
 /* OWNER CHECK */
-$check = mysqli_query($conn,
-"SELECT * FROM businesses WHERE id='$id' AND user_id='$user_id'");
+$check = mysqli_query($conn, "SELECT * FROM businesses WHERE id='$id' AND user_id='$user_id'");
 
 if(mysqli_num_rows($check) == 0){
     die("Not allowed");
@@ -26,16 +24,13 @@ if(mysqli_num_rows($check) == 0){
 
 /* IMAGE UPLOAD */
 $image_sql = "";
-
 if(!empty($_FILES['image']['name'])){
-
     $img = time()."_".$_FILES['image']['name'];
     move_uploaded_file($_FILES['image']['tmp_name'], "uploads/".$img);
-
     $image_sql = ", image='$img'";
 }
 
-/* UPDATE QUERY */
+/* UPDATE QUERY (Ab ismein error nahi aayega) */
 $sql = "UPDATE businesses SET 
 name='$name',
 phone='$phone',
@@ -44,8 +39,10 @@ description='$description'
 $image_sql
 WHERE id='$id' AND user_id='$user_id'";
 
-mysqli_query($conn,$sql);
-
-header("Location: my_business.php?updated=1");
+if(mysqli_query($conn, $sql)){
+    header("Location: my_business.php?updated=1");
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
 exit();
 ?>
